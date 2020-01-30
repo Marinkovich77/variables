9ecbbde76d03c35657d6aa975799309c12c4c4f0
Marinkovich77 <G00387874@gmit.it>
2020-01-30 22:41:59 +0000

    added new file for variables

---
+++ variables.py
@@ -0,0 +1,26 @@
+# Mario Stolba
+# Looking at variables
+
+x = 911.0
+y = 2
+
+z = x + y
+
+
+
+print(z)
+
+fac7 = 7 * 6 * 5 * 4 * 3 * 2 * 1
+
+print(fac7)
+
+b = True
+c = False
+
+s = "ian.mcloughlin@gmit.ie"
+print(s[0:14:2])
+
+
+l = ['a', 'b', 'c', 4, 5, 6, 7, 8, 9]
+
+print(l[2:6:3])
